738f206c06386512634c4775ecbb56965f8a3da5
Carsten Daurehøj <arelstone@gmail.com>
2023-08-08 13:50:29 +0200

    ✏️ Fix typo (#6)
    * ✏️ Fix typo
    
    * Add tokens
    
    * sdf
    
    * sdf
    
    * sdf
    
    * sdfdsf
    
    * asd
    
    * asd

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-yarn esbuild src/main.ts --bundle --outfile=lsr --allow-overwrite --target=node16 --format=cjs --platform=node
+yarn esbuild src/main.ts --bundle --outfile=lsr --allow-overwrite --target=node16 --format=cjs --platform=node \
+    --external:yargs
+
